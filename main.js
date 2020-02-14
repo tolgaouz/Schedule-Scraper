2c35c123f598b4c22eb5110e04d4ce25687a8f02
tolgaouz <oguzt16@itu.edu.tr>
2020-02-14 17:24:40 +0300

    added exhibition type

--- main.js
+++ main.js
@@ -7,48 +7,43 @@ const fs = require('fs');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  const base_url = 'https://schedule.sxsw.com';
 
-  await page.goto('https://schedule.sxsw.com/2020/films');
-  const film_urls = await page.evaluate(()=>{
+  await page.goto('https://schedule.sxsw.com/2020/03/13/events');
+  const urls_data = await page.evaluate(()=>{
     let events = document.getElementsByClassName('row single-event');
-    let urls = [];
+    let data = [];
     for(var i=0;i<events.length;i++){
-        urls.push(events[i].getElementsByTagName('div')[0].getElementsByTagName('a')[0].href);
+        let tmp = {};
+        tmp['URL'] = events[i].getElementsByTagName('div')[0].getElementsByTagName('a')[0].href;
+        tmp['Date'] = events[i].childNodes[1].getElementsByClassName('text-center')[0].textContent;
+        tmp['Event Type'] = events[i].childNodes[3].textContent;
+        data.push(tmp);
     }
-    return urls
+    return data
   });
-
-  await page.goto('https://schedule.sxsw.com/2020/events/type/showcase?days=all');
-  const showcase_urls = await page.evaluate(()=>{
-    let events = document.getElementsByClassName('row single-event');
-    let urls = [];
-    for(var i=0;i<events.length;i++){
-        urls.push(events[i].getElementsByTagName('div')[0].getElementsByTagName('a')[0].href);
-    }
-    return urls
-  });
-
 
   // Function below will be used for general scraping
   // Will return different data based on which url set is given
   
-   const get_data = (urls) => 
-   { return Promise.all(urls.map(async (url) => {
+   const get_data = (urls_data) => 
+   { return Promise.all(urls_data.map(async (url_data) => {
     // Options for request-promise function
     var options = {
-        uri: url,
+        uri: url_data['URL'],
         transform: function (body) {
             return cheerio.load(body);
         }
     };
     // Map each url object to a Promise by using request-promise package
     let out = rp(options).then($=>{
-        let screening = {};
+        let data = {};
         // If it is a screening URL 
-        screening['Film Screenings'] = [];
-        if ($('h3.uppercase').text().includes('Screening')) {
-            screening['Title'] = $('h1.event-name').text();
-            screening['Link'] = url
+        if (url_data['Event Type'].includes('Screening')) {
+            data['Film Screenings'] = [];
+            data['Title'] = $('h1.event-name').text();
+            data['Link'] = url_data['URL'];
+            data['Event Type'] = 'Screening';
             // get screening dates and venues
             $('div.event-sidebar').find('div.related-event > div.small-10').each((idx, el) => {
                 let tmp = {};
@@ -62,14 +57,14 @@ const fs = require('fs');
                 });
                 tmp['Venues'] = venues;
                 // Add the individual screening to film screenings
-                screening['Film Screenings'].push(tmp);
+                data['Film Screenings'].push(tmp);
             });
             // get tags
             let tags = [];
             $('.event-tags').find('a.tag').each((idx, el) => {
                 tags.push($(el).text());
             });
-            screening['Tags'] = tags;
+            data['Tags'] = tags;
             // get director
             // Contact + Credits Info Section
             $('div.screening-info > div.large-8 > div.body > div.credits').each((idx, el) => {
@@ -77,7 +72,7 @@ const fs = require('fs');
                 // is the header for that section
                 if ($(el).find('h3.uppercase').text().includes('Credits')) {
                     $(el).find('.row').each((i, elem) => {
-                        screening[$(elem).find('b').text().replace(':', '').trim()] = $(elem).text().replace($(elem).find('b').text(), '');
+                        data[$(elem).find('b').text().replace(':', '').trim()] = $(elem).text().replace($(elem).find('b').text(), '');
                     });
                     // If the header is Contact do the same as well
                     // these are treated different to locate them responsively and Credit fields have &nbsp; at the end
@@ -97,7 +92,7 @@ const fs = require('fs');
                                 return this.children.data;
                             }
                         }).get().join('');
-                        screening[$(elem).find('b').text().trim()] = t;
+                        data[$(elem).find('b').text().trim()] = t;
                     });
                 }
             });
@@ -105,11 +100,46 @@ const fs = require('fs');
             // Entry + Movie Info Section
             $('div.screening-info > div.large-4 > div.row').each((idx, el) => {
                 // Find b tags, get their text, replace : and trim just in case there are any spaces
-                screening[$(el).find('b').text().replace(':', '').trim()] = $(el).text().replace($(el).find('b').text(), '');
+                data[$(el).find('b').text().replace(':', '').trim()] = $(el).text().replace($(el).find('b').text(), '');
             });
-        
+    // If the event is an exhibition
+    }else if (url_data['Event Type'].includes('Exhibition')) {
+        data['Title'] = $('h1.event-name').text();
+        data['Link'] = url_data['URL'];
+        data['Event Type'] = 'Exhibition';
+        data['Venue'] = $('.venue-title').text();
+        data['Date'] = $('div.event-date').text();
+        data['Venue Size'] = $('div.venue-size').text();
+        data['Venue Address'] = $('div.venue-address').text();
+        // get tags
+        let tags = [];
+        $('.event-tags').find('a.tag').each((idx, el) => {
+            tags.push($(el).text());
+        });
+        data['Tags'] = tags;
+        data['Description'] = $('div.row description').find('div.large-8').text();
+        $('div.row description > div.large-4 > div.row').each((idx, el) => {
+            data[$(el).find('b').text().replace(':', '').trim()] = $(el).text().replace($(el).find('b').text(), '');
+        });
+        // People also favorited part
+        data['People Favorited'] = [];
+        $('div.related-event').each((idx,el)=>{
+            let tmp = {};
+            // People also favorited links ar designed in this way
+            // Title of the event + 'AT' + Venue
+            let full_text = $(el).find('div.small-10 > div.row > div.event-details').text().split('AT');
+            let title = full_text[0];
+            let venue = full_text[1];
+            tmp['Title'] = title;
+            tmp['Venue'] = venue;
+            // Link of the specific event
+            tmp['Link'] = base_url+$(el).find('div.small-10 > div.row > div.event-details > a')[0].attribs['href'];
+            tmp['Date'] = $(el).find('div.small-10 > div.row > div.thumbnail > small > div.date').text();
+            tmp['Time'] = $(el).find('div.small-10 > div.row > div.thumbnail > small > div.time').text();
+            data['People Favorited'].push(tmp);
+        });
     }
-    return screening
+    return data
     }).catch(e=>{
         console.log(e)
         return 'URL can not be reached'
@@ -118,7 +148,7 @@ const fs = require('fs');
     }))};
 
  // output to a json file
-    const films_data = await get_data(film_urls).then((val) =>{
+    const films_data = await get_data(urls_data).then((val) =>{
         var data = JSON.stringify(val);
         fs.writeFile('data.json', data, function(err, result) {
             if(err) console.log('error', err);
